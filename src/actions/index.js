3da3fbafac2b4ddab80bed4b2bda9cf2bfbfe6c8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-22 17:12:49 +0700

    Controlled search bar, added middleware

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+import config from '../../config.js'
+
+
+const API_KEY = config.WeatherAPI
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,11 +3,30 @@ import React from 'react';
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = { term: '' }
     }
+
+    onInputChange = (event) => {
+    	
+    	this.setState({
+    		term: event.target.value
+    	})
+    }
+
+    onFormSubmit = (event) => {
+    	event.preventDefault();
+    }
+
     render() {
         return (
-        	<form className="input-group">
-        		<input />
+        	<form onSubmit={this.onFormSubmit} className="input-group">
+        		<input
+        			placeholder="Get a five-day forecast in your favorite cities"
+        			className="form-control"
+        			value={this.state.term}
+        			onChange={this.onInputChange} 
+        		/>
         		<span className="input-group-btn">
         		<button type="submit" className="btn btn-secondary">Submit</button>
         		</span>
